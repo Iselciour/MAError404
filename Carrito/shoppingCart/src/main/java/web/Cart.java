a1be2cf8f56475e2cd2fbf70cbce64647e91ea74
Iselciour <84604993+Iselciour@users.noreply.github.com>
2021-10-05 22:02:58 -0500

    Add files via upload

---
+++ Carrito/shoppingCart/src/main/java/web/Cart.java
@@ -0,0 +1,51 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import model.Obj;
+
+@WebServlet("/Cart")
+public class Cart extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        HttpSession session = request.getSession();
+        
+        List<Obj> item = (List<Obj>) session.getAttribute("item");
+        if (item == null) {
+            item = new ArrayList<>();
+            session.setAttribute("item", item);
+        }
+        String addItem = request.getParameter("item");
+        if (addItem != null && !addItem.trim().equals("")) {
+            Boolean existing = true;
+            for(Obj a:item){
+                if(a.getName().equals(addItem)){
+                    a.setAv(a.getAv()+1);
+                    existing = false;
+                    break;
+                }
+            }
+            if (existing) {
+                Obj itemm = new Obj(addItem);
+                itemm.setAv(1);
+                item.add(itemm);
+            }
+        }
+        try ( PrintWriter out = response.getWriter()) {
+            out.print("<h1>Shopping Cart Wishlist</h1>");
+            out.print("<br>");
+            for (Obj itemm: item) {
+            out.print("<li>"+itemm.getName()+ "  "+ itemm.getAv()+"</li>");
+            }
+            
+            out.print("<br>");
+            out.print("<a href='/shoppingCart'>Return to Main Menu</a>");
+        }
+    }
+}
